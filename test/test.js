f13b525bc669edaca02e100450f2c814fa64de1e
Jesse Nayak <jessenayak@Jesses-MacBook.local>
2016-05-20 11:11:01 -0400

    Creating tests for markdown-it-prezi

--- test/test.js
+++ test/test.js
@@ -3,11 +3,11 @@
 var path = require('path');
 var generate = require('markdown-it-testgen');
 
-describe('markdown-it-video', function() {
+describe('markdown-it-prezi', function() {
   var md = require('markdown-it')({
     html: true,
     linkify: true,
     typography: true
   }).use(require('../'));
-  generate(path.join(__dirname, 'fixtures/video.txt'), md);
+  generate(path.join(__dirname, 'fixtures/prezi.txt'), md);
 });
